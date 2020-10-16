f145f473be256355161fe81726a41f49645b8a4b
Unknown <blossomperez18@gmail.com>
2020-10-16 18:04:18 -0500

    Add user interface and methods

--- src/ContactApp/App.java
+++ src/ContactApp/App.java
@@ -1,32 +1,44 @@
 package ContactApp;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App {
-
+    
     public static void main(String[] args) throws Exception {
         Input input = new Input();
         String directoryName = "data";
         String fileName = "contacts.txt";
-        Path dataFilePath = Contact.createDirectoryAndFile(directoryName, fileName);
-        userInterface();
-        int userChoice = Input.getInt(1,5, "Enter an option (1, 2, 3, 4 or 5):");
-        switch (userChoice){
-
-        }
-
+        
+        
         try{
-
+            Path dataFilePath = Contact.createDirectoryAndFile(directoryName, fileName);
+            List<String> contacts = Arrays.asList("Jack Blank : 2105678923", "Jane Doe : 7898902", "Sam Space : 2105818123");
+            Files.write(dataFilePath, contacts);
+            userInterface();
+            int userChoice = Input.getInt(1, 5, "Enter an option (1, 2, 3, 4 or 5):");
+          
+    
 
         } catch (IOException ex){
-
-        };
+            System.out.println("Cannot create the file.");
+            ex.printStackTrace();
+        }
+        
+     
 
 
     }
+ 
 
     public static void userInterface(){
         System.out.println("1. View contacts.\n" +
@@ -36,24 +48,38 @@ public class App {
                 "5. Exit.\n");
     }
 
-    public static void displayUserSelection(int option) {
+    public static void displayUserSelection() {
+        Input input = new Input();
+        int option = input.getInt(min, max, prompt);
         switch(option) {
             case 1:
-                Contact.printFileContents(dataFilePath);
+                Contact.printFileContents();
                 break;
             case 2:
-                Contact.addContact();
+           
+               
                 break;
             case 3:
-                Contact.searchContact(userInput);
+                Contact.searchContact();
                 break;
             case 4:
-                Contact.deleteContact(dataFilePath, userInput);
+                Contact.deleteContact();
                 break;
             case 5:
                 System.out.println("Goodbye");
                 System.exit(0);
                 break;
         }
+
+        boolean confirm = Input.yesNo("Would you like to select another option? [y/n]");
+        if(confirm) {
+            userInterface();
+            int userChoice = Input.getInt(1,5, "Enter an option (1, 2, 3, 4 or 5):");
+            displayUserSelection(userChoice);
+        }else{
+            System.out.println("Goodbye");
+            System.exit(0);
+        }
     }
+ 
 }
