7a8d62b60ef0cb55105a5805dc9f0308f32fadcb
Unknown <blossomperez18@gmail.com>
2020-10-18 20:55:31 -0500

    Refactor methods

--- src/ContactApp/Contact.java
+++ src/ContactApp/Contact.java
@@ -35,68 +35,59 @@ public class Contact {
         this.contactNumber = number;
     }
 
-
-
-    public Path createDirectoryAndFile() throws IOException {
-        if(Files.notExists(dataFilePath)){
-            Files.createDirectories(dataFilePath);
-        }
-        if(!Files.exists(dataFilePath)){
-            Files.createFile(dataFilePath);
-        }
-        return dataFilePath;
-    }
-
     public static void printFileContents() throws IOException {
         List<String> fileContents = Files.readAllLines(dataFilePath);
         System.out.println();
-        System.out.printf("%-20s | %-20s%n", "Name", "Phone Number");
+        System.out.printf("%-20s | %-s |%n", "Name", "Phone Number");
         System.out.println("-------------------------------------------");
         for(int i = 0; i < fileContents.size(); i++){
             System.out.printf("%s%n", fileContents.get(i));
         }
     }
 
+    public static String formatPhoneNum(String aNum){
+        String phoneNumber = "";
+        if(aNum.length() == 10){
+            phoneNumber = String.format("%s-%s-%s", aNum.substring(0,3), aNum.substring(3,6), aNum.substring(6));
+        }else if(aNum.length() == 7){
+            phoneNumber =String.format("%s-%s-%s", "xxx", aNum.substring(0,3), aNum.substring(3));
+        }
+        return phoneNumber;
+    }
+
 
     public static void addContact() throws IOException{
         Input entry = new Input();
-        System.out.println("Enter your new contacts name: ");
+        System.out.println("\nEnter your new contacts name: ");
         String name = entry.getString();
+        System.out.println("\nEnter new contact's phone number:");
+        String number = formatPhoneNum(entry.getString());
+        Files.write(
+            dataFilePath, 
+            Arrays.asList(String.format("%-20s | %s |", name, number)), 
+            StandardOpenOption.APPEND
+        );
+        System.out.print("Contact has been added");
+        printFileContents();
         System.out.println();
-        System.out.println("Enter new contact's phone number:");
-        String number = entry.getString();
-        Files.write(dataFilePath, Arrays.asList(String.format("%-20s | %-20s", name, number)), StandardOpenOption.APPEND);
-        System.out.print("Contact Added");
     }
 
-    //Needs modified
-    public void updateContact(Path filePath, String oldValue, String newValue) throws IOException {
-        List<String> fileContents = Files.readAllLines(filePath);
-        List<String> modifiedList = new ArrayList<>();
-        for (String item: fileContents){
-            if (item.equals(oldValue)){
-                modifiedList.add(newValue);
-
-            } else {
-                modifiedList.add(item);
-            }
-        }
-        Files.write(filePath, modifiedList);
-    }
-
-    //Needs modified
-    public void deleteContact() throws IOException {
+    public static void deleteContact() throws IOException {
         Input entry = new Input();
         System.out.println("Which entry would you like to delete?");
-        entry.getString();
+        String deletedValue = entry.getString().toLowerCase();
         List<String> fileContents = Files.readAllLines(dataFilePath);
         List<String> modifiedList = new ArrayList<>();
+        System.out.println();
         for (String item: fileContents){
-            if(!item.equals(entry)){
+            if(!item.toLowerCase().contains(deletedValue)){
                 modifiedList.add(item);
             }
         }
         Files.write(dataFilePath, modifiedList);
+        System.out.print("Contact has been deleted");
+        printFileContents();
+        System.out.println();
 
     }
 
@@ -105,14 +96,12 @@ public class Contact {
         System.out.println("Search for contact:");
         String search = entry.getString();
         List<String> fileContents = Files.readAllLines(dataFilePath);
-        List<String> modifiedList = new ArrayList<>();
         System.out.println();
         for(String contact: fileContents) {
-            if(contact.equals(search)) {
-                modifiedList.add(contact);
+            if(contact.toLowerCase().contains(search.toLowerCase())) {
+                System.out.println(contact);
             }
         }
-        Files.write(dataFilePath, modifiedList);
 
     }
 
